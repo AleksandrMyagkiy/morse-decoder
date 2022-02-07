0f811a2bf0bf5705ae49d7912e54094d4e51be7a
Aleksandr <dentranscom@gmail.com>
2022-02-07 13:56:26 +0200

    feat: solution of the morse code

--- src/index.js
+++ src/index.js
@@ -38,9 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    
+    let morseStr = '',
+        morseRes ='';
+    for (let i = 0; i < expr.length; i += 10){
+        morseStr = expr.slice(i, i + 10);
+        if (morseStr == "*".repeat(10)) {
+            morseRes += ' ';
+            continue;
+        }
+        let result = '',
+            morseWord = '';
+        for(let k = 0; k<morseStr.length; k += 2){
+            morseWord = morseStr.slice(k, k + 2);
+            if(morseWord == '10') {
+                result += '.';
+            }
+            if(morseWord == "11") {
+                result += '-';
+            }
+        }
+        morseRes += MORSE_TABLE[result];
+    }
+    return morseRes;
 }
 
 module.exports = {
     decode
-}
+};
